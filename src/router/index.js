774571a4fdb4df76e1e47d9cca2d41b8985a9bd3
worldsong <2060884089@qq.com>
2017-10-30 22:27:28 +0800

    html需求转vue页面实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '@/views/GoodsList'
+import Cart from '@/views/Cart'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/GoodsList',
+      name: 'GoodsList',
+      component: GoodsList
+    },
+    {
+      path: '/Cart',
+      name: 'Cart',
+      component: Cart
     }
   ]
 })

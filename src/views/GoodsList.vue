a250c9956481cae394d0f99d0d78ccd819d7242f
worldsong <2060884089@qq.com>
2017-11-01 20:46:06 +0800

    样式拆分

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -55,9 +55,6 @@
 
 <script>
   import './../assets/css/base.css'
-  import './../assets/css/nav-header.css'
-  import './../assets/css/nav-bread.css'
-  import './../assets/css/nav-footer.css'
   import './../assets/css/goods-list.css'
   import NavHeader from './../components/NavHeader.vue'
   import NavFooter from './../components/NavFooter.vue'

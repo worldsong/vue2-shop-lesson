c47379b09e27956ecb4e1591e6d7fa2aee6021b3
worldsong <2060884089@qq.com>
2017-11-07 21:17:41 +0800

    连接MongoDB，商品列表查询接口

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var goods = require('./routes/goods');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/goods', goods);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('用户路由');
-});
-
-module.exports = router;

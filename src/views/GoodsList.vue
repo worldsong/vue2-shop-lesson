4bd4c05bad04006e07080025fc7fa99b41702b8d
worldsong <2060884089@qq.com>
2017-11-07 22:16:30 +0800

    商品列表分页,前端页面传参axios->params

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -67,6 +67,9 @@
         data() {
             return {
                 goodsList:[],
+                sortFlag: 1,
+                page: 1,
+                pageSize: 8,
                 priceFilter:[
                   {
                     startPrice:'0.00',
@@ -108,7 +111,14 @@
         },
         methods: {
           getGoodsList(){
-            axios.get("http://localhost:3000/goods").then((result) => {
+            var param = {
+              page: this.page,
+              pageSize: this.pageSize,
+              sort: this.sortFlag
+            }
+            axios.get("http://localhost:3000/goods",{
+              params:param
+            }).then((result) => {
               console.log(result.data.result)
               var res = result.data;
               this.goodsList = res.result.list;

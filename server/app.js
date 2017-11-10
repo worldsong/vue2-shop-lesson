739beae0683418193c86ff5520aa80549e873fa9
worldsong <2060884089@qq.com>
2017-11-10 22:17:59 +0800

    登录拦截

--- server/app.js
+++ server/app.js
@@ -35,6 +35,23 @@ app.all('*', function(req, res, next) {
   next();
 });
 
+app.use(function (req,res,next) {
+  if(req.cookies.userId){
+    next();
+  }else{
+    console.log("url:"+req.originalUrl);
+    if(req.originalUrl=='/users/login' || req.originalUrl=='/users/logout' || req.originalUrl.indexOf('/goods/list')>-1){
+      next();
+    }else{
+      res.json({
+        status:'10001',
+        msg:'当前未登录',
+        result:''
+      });
+    }
+  }
+});
+
 app.use('/', index);
 app.use('/goods', goods);
 app.use('/users', users);

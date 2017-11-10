7ff0a77976a42c3c41b2d7799a58e9ef7a3046b3
worldsong <2060884089@qq.com>
2017-11-10 21:07:01 +0800

    商品加入购物车

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -66,4 +66,76 @@ router.get('/', function(req, res, next) {
   })
 });
 
+//加入到购物车
+router.post("/addCart", function (req,res,next) {
+  var userId = '100000077',productId = req.body.productId;
+  var User = require('../models/user');
+  User.findOne({userId:userId}, function (err,userDoc) {
+    if(err){
+      res.json({
+        status:"1",
+        msg:err.message
+      })
+    }else{
+      // console.log("userDoc:"+userDoc);
+      if(userDoc){
+        var goodsItem = '';
+        userDoc.cartList.forEach(function (item) {
+          if(item.productId == productId){
+            //购物车商品已经存在，只做数量+1
+            goodsItem = item;
+            item.productNum ++;
+          }
+        });
+        if(goodsItem){
+          userDoc.save(function (err2,doc2) {
+            if(err2){
+              res.json({
+                status:"1",
+                msg:err2.message
+              })
+            }else{
+              res.json({
+                status:'0',
+                msg:'',
+                result:'suc'
+              })
+            }
+          })
+        }else{
+          //新增购物车商品
+          Goods.findOne({productId:productId}, function (err1,doc) {
+            if(err1){
+              res.json({
+                status:"1",
+                msg:err1.message
+              })
+            }else{
+              if(doc){
+                doc.productNum = 1;
+                doc.checked = 1;
+                userDoc.cartList.push(doc);
+                userDoc.save(function (err2,doc2) {
+                  if(err2){
+                    res.json({
+                      status:"1",
+                      msg:err2.message
+                    })
+                  }else{
+                    res.json({
+                      status:'0',
+                      msg:'',
+                      result:'suc'
+                    })
+                  }
+                })
+              }
+            }
+          });
+        }
+      }
+    }
+  })
+});
+
 module.exports = router;

46bf28faf735b867800629d83056d318811c4419
worldsong <2060884089@qq.com>
2017-11-14 21:07:57 +0800

    模态框组件封装及使用

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -54,6 +54,26 @@
           </div>
         </div>
       </div>
+      <modal v-bind:mdShow="mdShow" v-on:close="closeModal">
+        <p slot="message">
+          请先登录,否则无法加入到购物车中!
+        </p>
+        <div slot="btnGroup">
+          <a class="btn btn--m" href="javascript:void(0);" @click="mdShow = false">关闭</a>
+        </div>
+      </modal>
+      <modal v-bind:mdShow="mdShowCart" v-on:close="closeModal">
+        <p slot="message">
+          <svg class="icon-status-ok">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-status-ok"></use>
+          </svg>
+          <span>加入购物车成!</span>
+        </p>
+        <div slot="btnGroup">
+          <a class="btn btn--m" href="javascript:void(0);" @click="mdShowCart = false">继续购物</a>
+          <router-link class="btn btn--m btn--red" href="javascript:;" to="/cart">查看购物车</router-link>
+        </div>
+      </modal>
       <div class="md-overlay" v-show="overLayFlag" @click.stop="closePop"></div>
       <nav-footer></nav-footer>
       </body>
@@ -66,6 +86,8 @@
   import NavHeader from './../components/NavHeader.vue'
   import NavFooter from './../components/NavFooter.vue'
   import NavBread from './../components/NavBread.vue'
+  import Modal from './../components/Modal'
+
   import axios from 'axios'
 
     export default {
@@ -78,6 +100,8 @@
                 pageSize: 8,
                 busy:true,
                 loading:false,
+                mdShow: false,
+                mdShowCart: false,
                 priceFilter:[
                   {
                     startPrice:'0.00',
@@ -115,7 +139,8 @@
         components:{
           NavHeader,
           NavFooter,
-          NavBread
+          NavBread,
+          Modal
         },
         methods: {
           getGoodsList(flag){
@@ -179,10 +204,16 @@
               var res = res.data;
               if(res.status==0){
                 alert("加入成功");
+                this.mdShowCart = true;
               }else{
-                alert("Error msg:" + res.msg );
+//                alert("Error msg:" + res.msg );
+                this.mdShow = true;
               }
             })
+          },
+          closeModal(){
+            this.mdShow = false;
+            this.mdShowCart = false;
           },
           showFilterPop(){
             this.filterBy = true;
@@ -191,6 +222,7 @@
           closePop(){
             this.filterBy = false;
             this.overLayFlag = false;
+            this.mdShowCart = false;
           }
         }
     }

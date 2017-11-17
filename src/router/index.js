22d7270d1dc4ddb271aa0dd09483ddaaf5d6f869
worldsong <2060884089@qq.com>
2017-11-17 22:13:00 +0800

    地址模块设计及实现(地址列表渲染、设置默认地址、删除地址)；至少保留一条收货地址！

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from '@/views/GoodsList'
 import Cart from '@/views/Cart'
+import Address from '@/views/Address'
 
 Vue.use(Router)
 
@@ -21,6 +22,11 @@ export default new Router({
       path: '/Cart',
       name: 'Cart',
       component: Cart
+    },
+    {
+      path: '/Address',
+      name: 'Address',
+      component: Address
     }
   ]
 })
